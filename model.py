8e3d04c19887a50feb3d212b308670e6d05f409f
Oladimeji Williams <81469074+Oladimeji-Williams@users.noreply.github.com>
2021-09-15 11:29:45 +0100

    Update model.py

--- model.py
+++ model.py
@@ -2,11 +2,10 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 #import the dataset
 X_train= pd.read_csv(r"X_train.csv")
 y_train= pd.read_csv(r"y_train.csv")
 
 reg = RandomForestClassifier(random_state=0)
-reg.fit(X_train, y_train)
+reg.fit(X_train, np.ravel(y_train))
