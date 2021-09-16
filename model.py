34817c319d6cb5e4c4891ace9c9071a2dbe86b55
Oladimeji Williams <81469074+Oladimeji-Williams@users.noreply.github.com>
2021-09-17 00:42:02 +0100

    Update model.py

--- model.py
+++ model.py
@@ -4,8 +4,9 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 #import the dataset
-X_train= pd.read_csv(r"X_train.csv")
-y_train= pd.read_csv(r"y_train.csv")
+X_train= pd.read_csv(r"X_train_cleaned.csv")
+y_train= pd.read_csv(r"y_train_cleaned.csv")
 
+#Build the model
 reg = RandomForestClassifier(random_state=0)
 reg.fit(X_train, np.ravel(y_train))
